dba1ee06868765a9d42e7cecc0cd794744d54c45
kuanchiliao1 <kuanchiliao@gmail.com>
2024-07-23 15:20:15 -0700

    fix: update setup script to work with linux
    Co-authored-by: Garrett <gjcochran@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 #set -x # testing
@@ -8,42 +8,48 @@ error_handler() {
     local command=$2
     local error_code=${3:-1}
     echo "Error occurred in command '$command' on line $line. Exit code: $error_code" >&2
-    say "helios infrastructure setup failed"
+    which say >/dev/null 2>&1 && say "helios infrastructure setup failed"
     exit $error_code
 }
 
-trap 'error_handler ${LINENO} "$BASH_COMMAND" $?' ERR
+if [ -n "$BASH_VERSION" ]; then
+    trap 'error_handler ${LINENO} "$BASH_COMMAND" $?' ERR
+fi
 
 main() {
     echo "Starting Helios infrastructure setup..."
 
-    if [ -z "$1" ]; then
-      echo "Profile name is required as the first argument."
-      exit 1
+    if [ $# -eq 0 ]; then
+        echo "Profile name is required as the first argument."
+        exit 1
     fi
 
-    PROFILE=$1
-    # read -p "Enter the IAM profile name: " PROFILE
+    PROFILE="$1"
     echo "1-Using profile: $PROFILE"
-    export AWS_PROFILE=$PROFILE
+    export AWS_PROFILE="$PROFILE"
 
-    chmod +x setup_cdk.sh assume_role.sh
+    if [ -f setup_cdk.sh ] && [ -f assume_role.sh ]; then
+        chmod +x setup_cdk.sh assume_role.sh
+    else
+        echo "setup_cdk.sh or assume_role.sh not found in the current directory."
+        exit 1
+    fi
     
     echo "Running setup_cdk.sh..."
-    bash ./setup_cdk.sh $PROFILE
+    ./setup_cdk.sh "$PROFILE"
     
     echo "Deploying IamStack..."
     cdk deploy IamStack --require-approval never
     
     echo "Assuming deployment role..."
-    bash ./assume_role.sh $PROFILE
+    ./assume_role.sh "$PROFILE"
     
     echo "Deploying all stacks..."
-    if cdk deploy VpcStack --require-approval never; then
-        say "helios infrastructure setup done"
+    if cdk deploy --all --require-approval never; then
+        which say >/dev/null 2>&1 && say "helios infrastructure setup done"
         echo "Deployment completed successfully!"
     else
-        say "helios infrastructure setup failed"
+        which say >/dev/null 2>&1 && say "helios infrastructure setup failed"
         echo "Deployment failed. Check the logs for more information." >&2
         exit 1
     fi
