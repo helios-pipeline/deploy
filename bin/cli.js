d6490e537364894e7a32fd0106f456dd44e3a45c
gjcochran <garrettjcochran@gmail.com>
2024-07-26 08:56:05 -0600

    feat: prompt user in cli for chatgpt api key provide this key to the flask ec2 .env so that frontend can use for quarantine analysis feature
    Co-authored-by: jamesdrabinsky <jamesdrabinsky@users.noreply.github.com>
    Co-authored-by: Kuanchiliao1 <Kuanchiliao1@users.noreply.github.com>

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,41 @@
 #!/usr/bin/env node
-const process = require('process');
+const process = require("process");
+const readline = require("readline/promises");
+const path = require("path");
+const fs = require("fs/promises");
 const { outputValidArguments } = require("../src/utils/valid-arguments");
 const { setProfile } = require("../src/utils/set-profile");
 const { runDeploy } = require("../src/utils/run-deploy");
 const { runDestroy } = require("../src/utils/run-destroy.js");
 const { heliosArt } = require("../src/ascii/heliosAscii.js");
+const { input, confirm } = require("@inquirer/prompts");
+
+
+async function getChatGPTApiKey() {
+  const wantApiKey = await confirm({
+    message: "Would you like to provide a ChatGPT API key?",
+  });
+
+  if (wantApiKey) {
+    const apiKey = await input({
+      message: "Please enter your ChatGPT API key:",
+    });
+    console.log(apiKey);
+
+    const envPath = path.resolve(process.cwd(), ".env");
+    await fs.appendFile(envPath, `\nchatGptApiKey=${apiKey}`);
+    return apiKey;
+  } else {
+    return null;
+  }
+}
 
 
 let profile;
 
 async function setup() {
-  // heliosArt();
   profile = await setProfile();
+  await getChatGPTApiKey();
   await runDeploy(profile);
 }
 
@@ -19,17 +43,16 @@ async function main() {
   const arg = process.argv[2];
   if (arg === undefined) {
     setup();
-    // outputValidArguments();
-  } else if ( arg === 'deploy') {
+  } else if (arg === "deploy") {
     heliosArt();
     setup();
-  } else if (arg === 'destroy') {
+  } else if (arg === "destroy") {
     if (!profile) {
       profile = await setProfile();
     }
     runDestroy(profile);
   } else {
-    console.log('Invalid argument.');
+    console.log("Invalid argument.");
     outputValidArguments();
   }
 }
